ad962419dc8039ad518c4f6bff9a83bad15ea0cf
Pavel Linkesch <pavel.linkesch@gmail.com>
2015-05-16 13:56:06 +0200

    show error when ajax fails

--- client/js/main.js
+++ client/js/main.js
@@ -10,28 +10,35 @@ $(function () {
 		e.preventDefault();
 
 		$results.html('<span class="fa fa-spinner fa-spin"></span> Loading...');
-		$.get('http://localhost:3000/get/'+ owner +'/'+ repo, function (data) {
+		$.ajax({
+			url: 'http://localhost:3000/get/'+ owner +'/'+ repo,
+			dataType: 'json',
+			success: function (data) {
 
-			if (data.code && data.code === 'error') {
-				$results.html('<div class="alert alert-danger">'+ data.message +'</div>');
-				return;
+				if (data.code && data.code === 'error') {
+					$results.html('<div class="alert alert-danger">'+ data.message +'</div>');
+					return;
+				}
+
+				var $list = $('<ul></ul>');
+				$results.html('<h2>Top 10 users who starred your repo:</h2>');
+				$results.append($list);
+
+			 	$.each(data, function (key, value) {
+			 		$list.append('<li>'+
+			 			'<a href="https://github.com/'+ value.name +'">'+
+			 				'<img src="'+ value.avatar +'" alt="'+ value.name  +'">'+
+			 				'<span class="name">'+ value.name +':</span>'+
+			 			'</a> '+
+			 			'<span class="stars">'+ value.stars +' <span class="fa fa-star"></span></span>'+
+			 		'</li>');
+			 	});
+
+			},
+			error: function () {
+				$results.html('<div class="alert alert-danger">An unexpected error occured.</div>');
 			}
-
-			var $list = $('<ul></ul>');
-			$results.html('<h2>Top 10 users who starred your repo:</h2>');
-			$results.append($list);
-
-		 	$.each(data, function (key, value) {
-		 		$list.append('<li>'+
-		 			'<a href="https://github.com/'+ value.name +'">'+
-		 				'<img src="'+ value.avatar +'" alt="'+ value.name  +'">'+
-		 				'<span class="name">'+ value.name +':</span>'+
-		 			'</a> '+
-		 			'<span class="stars">'+ value.stars +' <span class="fa fa-star"></span></span>'+
-		 		'</li>');
-		 	});
-
-		}, 'json');
+		});
 	});
 
 });
